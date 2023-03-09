06f2a18a1d220220a4a2bf66cd3533ae8dbc6e4f
suman-samanta <sumantheslayer99@gmail.com>
2023-03-09 10:34:32 +0530

    initial commit

---
+++ routes/auth.js
@@ -0,0 +1,57 @@
+const  router=require("express").Router();
+const User=require("../models/User");
+const bcrypt = require('bcrypt');
+
+
+// REGISTER
+router.post("/register",async(req,res)=>{
+    try{
+       const salt=await bcrypt.genSalt(10);
+       const hashedPass=await bcrypt.hash(req.body.password,salt) 
+       const newUser= new User({
+           username:req.body.username,
+           email:req.body.email,
+           mobile:req.body.mobile,
+           password:hashedPass
+       })
+
+       const user=await newUser.save();
+       const {password,...others}=user._doc
+
+       res.status(200).json(others);
+       console.log("New User Has Been created")
+    }catch(err){
+        res.status(500).json(err);
+        console.log(err);
+    }
+})
+
+// LOGIN 
+
+router.post("/login",async(req,res)=>{
+    try{
+        const user= await User.findOne({email:req.body.email},
+            // {username:req.body.username}
+            );
+        
+        if(!user){
+            res.status(400).json("Wrong Credentials !")
+            }
+        else{
+            const validated=await bcrypt.compare(req.body.password,user.password);
+
+            if( !validated){
+                res.status(400).json("Wrong Credentials !")
+                }else{
+                    const {password,...others}=user._doc;
+                    res.status(200).json(others);
+                }
+            }
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+module.exports=router;
